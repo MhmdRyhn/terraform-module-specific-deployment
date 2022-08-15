0887b7f893fce891f1f69ffe50da5d3d9fd3c00f
MhmdRyhn <rayhan.cse12@gmail.com>
2022-08-15 19:29:06 +0600

    feature: Update build.sh script (just the commands)

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 # This script can be used as needed. I just put the following commands to track the terraform commands
 # for the POC about the module specific deployment.
 
-# AWS_PROFILE=***** DEFAULT_REGION=eu-west-1 terraform plan -target="module.module_one" -out="tfplan"
-# AWS_PROFILE=***** DEFAULT_REGION=eu-west-1 terraform plan -target="module.module_two" -out="tfplan"
+# AWS_PROFILE=***** DEFAULT_REGION=eu-west-1 terraform plan -target="module.module_one" --var-file=inputs_value.tfvars -out="tfplan"
+# AWS_PROFILE=***** DEFAULT_REGION=eu-west-1 terraform plan -target="module.module_two" --var-file=inputs_value.tfvars -out="tfplan"
 # AWS_PROFILE=***** DEFAULT_REGION=eu-west-1 terraform apply "tfplan"
-# AWS_PROFILE=***** DEFAULT_REGION=eu-west-1 terraform destroy -target="module.module_one" --auto-approve
-# AWS_PROFILE=***** DEFAULT_REGION=eu-west-1 terraform destroy -target="module.module_two" --auto-approve
+# AWS_PROFILE=***** DEFAULT_REGION=eu-west-1 terraform destroy -target="module.module_one" --var-file=inputs_value.tfvars --auto-approve
+# AWS_PROFILE=***** DEFAULT_REGION=eu-west-1 terraform destroy -target="module.module_two" --var-file=inputs_value.tfvars --auto-approve
--- terraform/inputs.tf
+++ terraform/inputs.tf
@@ -2,3 +2,13 @@ variable "prefix" {
   type        = string
   description = "A prefix that will be used with all the resource name."
 }
+
+//variable "default_profile" {
+//  type = string
+//  description = "Default profile name"
+//}
+//
+//variable "region" {
+//  type = string
+//  description = "Default AWS region the resources to be deployed."
+//}
